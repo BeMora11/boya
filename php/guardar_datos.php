5449cb0cc28e5146b181f0b7e1ad4d92c325b1b0
BeMora11 <dronemaxis_935@outlook.com>
2021-04-07 16:09:09 -0500

    Version final

--- php/guardar_datos.php
+++ php/guardar_datos.php
@@ -2,12 +2,14 @@
 include_once 'conexion.php';
 $conexion = new DB();
 
-$oxigeno = $_GET['oxigeno'];
-$temperatura = $_GET['temp'];
-$turbidez = $_GET['turbidez'];
-$co2 = $_GET['co2'];
+$oxigeno = $_POST['oxigen'];
+$temperatura = $_POST['temp'];
+$turbidez = $_POST['turbidez'];
+$co2 = $_POST['dioxido'];
+$fecha = date('Y-m-d H:i:s');
 
-$query = $conexion->connect()->prepare("INSERT INTO dispositivo(oxigeno ,temperatura, turbidez, dioxido_carbono) VALUES(:oxigeno ,:temperatura, :turbidez, :dioxido_carbono)");
+$query = $conexion->connect()->prepare("INSERT INTO dispositivo(fecha, oxigeno ,temperatura, turbidez, dioxido_carbono) VALUES(:fecha, :oxigeno ,:temperatura, :turbidez, :dioxido_carbono)");
+$query->bindParam("fecha", $fecha, PDO::PARAM_STR);
 $query->bindParam("oxigeno", $oxigeno, PDO::PARAM_INT);
 $query->bindParam("temperatura", $temperatura, PDO::PARAM_INT);
 $query->bindParam("turbidez", $turbidez, PDO::PARAM_INT);
